f2b673f69a9f40836b9e7b808f0cd6cc47b5184d
Jonas Westman <jonaswestman@hotmail.com>
2024-10-02 20:42:57 +0200

    example with 2 containers

--- src/ui/ui_manager.cpp
+++ src/ui/ui_manager.cpp
@@ -14,33 +14,58 @@ void UIManager::Button1ClickHandler() {
 
 UIManager::UIManager(int screenWidth, int screenHeight)
 {
-	Container* panel = new Container(100, 100, 300, 300);
-	GridLayout* layout = new GridLayout(4);
-	panel->SetLayout(layout);
+	Container* panel1 = new Container(100, 100, 300, 300);
 
 	Button* button1 = new Button(0, 0, 200, 50, "Click me 1");
 	button1->SetClickCallback([this]() {
 		Button1ClickHandler();
 	});
 
-	Button* button2 = new Button(0, 0, 200, 50, "Click me 2");
+	Button* button2 = new Button(0, 100, 200, 50, "Click me 2");
 	button2->SetClickCallback([]() {
 		std::cout << "Button 2 clicked" << std::endl;
 	});
 
-	Button* button3 = new Button(0, 0, 200, 50, "Click me 3");
+	Button* button3 = new Button(0, 200, 200, 50, "Click me 3");
 	button3->SetClickCallback([]() {
 	std::cout << "Button 3 clicked" << std::endl;
 	});
 
-	TextLabel* textlabel = new TextLabel(200, 400, "hejsan");
+	TextLabel* textlabel1 = new TextLabel(200, 400, "hejsan");
 
-	panel->AddChild(button1);
-	panel->AddChild(button2);
-	panel->AddChild(button3);
-	panel->AddChild(textlabel);
+	panel1->AddChild(button1);
+	panel1->AddChild(button2);
+	panel1->AddChild(button3);
+	panel1->AddChild(textlabel1);
 
-	AddWidget(panel);
+	Container* panel2 = new Container(400, 100, 300, 300);
+	GridLayout* layout = new GridLayout(2);
+	panel2->SetLayout(layout);
+
+	Button* button4 = new Button(0, 0, 200, 50, "Click me 4");
+	button4->SetClickCallback([this]() {
+		Button1ClickHandler();
+		});
+
+	Button* button5 = new Button(0, 100, 200, 50, "Click me 5");
+	button5->SetClickCallback([]() {
+		std::cout << "Button 5 clicked" << std::endl;
+		});
+
+	Button* button6 = new Button(0, 200, 200, 50, "Click me 6");
+	button6->SetClickCallback([]() {
+		std::cout << "Button 6 clicked" << std::endl;
+		});
+
+	TextLabel* textlabel2 = new TextLabel(200, 400, "hejsan");
+
+	panel2->AddChild(button4);
+	panel2->AddChild(button5);
+	panel2->AddChild(button6);
+	panel2->AddChild(textlabel2);
+
+	AddWidget(panel1);
+	AddWidget(panel2);
 }
 
 void UIManager::AddWidget(Widget* widget) {
